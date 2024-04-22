690ef5c0a542a6c1bc63252d4e6d4c1b53edc980
Jyotish121 <jyotishmht18112001@gmail.com>
2024-04-22 13:15:02 +0530

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+ const countvalue = document.querySelector('#counter');
+
+
+ const increment = () => {
+    // get the value from UI
+    let value = parseInt(countvalue.innerText);
+    //update the value
+    value = value +1;
+    //set the value onto UI
+    countvalue.innerText = value;
+ }
+
+ const decrement = () => {
+    // get the value from UI
+    let value = parseInt(countvalue.innerText);
+    //update the value
+    value = value - 1;
+    //set the value onto UI
+    countvalue.innerText = value;
+ }
